a021a8a256f47d35180d61ce6e2464957cf8fa36
KINGG777 <pramodbehera777@gmail.com>
2025-02-04 23:32:10 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://a63d02b2fe9bf4c10acd49e34ad410dc-764428228.us-east-1.elb.amazonaws.com";
+const API_BASE_URL = "ok.kingg.tech";
 export default API_BASE_URL;
